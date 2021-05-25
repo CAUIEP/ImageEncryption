08dd5a2bc99cb2e67984e6b90869c10009c3757e
DOHYUN <pastapeter@naver.com>
2021-05-25 17:48:31 +0900

    update urls(upload, confirm, detail)

--- IEP/Customer/urls.py
+++ IEP/Customer/urls.py
@@ -5,6 +5,9 @@ from .views import *
 app_name = 'Customer'
 urlpatterns = [
     path('', customer_home, name="customer_home"),
+    path('confirm/', customer_confirm, name="customer_confirm"),
+    path('detail/<int:pk>/', customer_read, name="customer_read"),
+    path('upload/<int:pk>/', picture_upload, name="picture_upload"),
     path("signup/", sign_up, name="signup"),
     path("login/", login, name="login"),
     path("logout/", logout, name="logout"),
