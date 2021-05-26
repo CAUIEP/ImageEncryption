aaa28de29749899acf591e78383ee33e0a617675
yb5464 <kyb3634@naver.com>
2021-05-27 00:00:55 +0900

    url IEP

--- IEP/IEP/urls.py
+++ IEP/IEP/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     path('', home, name="home"),
     path('customer/', include("Customer.urls")),
     path('clerk/', include("Clerk.urls")),
+    path('picturehandler/', include("Picture_Handler.urls"))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
