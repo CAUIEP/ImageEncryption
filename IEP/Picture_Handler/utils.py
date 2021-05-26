c1d160234e285ec99607e7405510565971450d6a
yb5464 <kyb3634@naver.com>
2021-05-27 00:49:40 +0900

    define picture decryptor

--- IEP/Picture_Handler/utils.py
+++ IEP/Picture_Handler/utils.py
@@ -37,3 +37,43 @@ class PictureEncryptor:
         os.remove(image_path)
 
         return encrypt_file
+
+
+class PictureDecryptor:
+    #Decrypting Image
+    @classmethod
+    def decrypt_image(cls, image, key):
+        key_value = '1'
+        key_value = key_value.encode()
+        digest = hashlib.md5(key_value).digest() # 16 byte binary
+
+        key = digest
+        iv=b'0000000000000000'
+
+        image = str(image)
+        enc_file2 = open(image,"rb")
+        enc_data2 = enc_file2.read()
+        enc_file2.close()
+
+        cfb_decipher = AES.new(key, AES.MODE_CFB, iv)
+        plain_data = (cfb_decipher.decrypt(enc_data2))
+
+        decrypt_file = image.split(".")
+        decrypt_file = decrypt_file[:-1][0] + ".jpg"
+
+        imageStream = io.BytesIO(plain_data)
+        try:
+            #파일이 열리는 지 확인하는 메서드 
+            imageFile = PIL.Image.open(imageStream)
+        except:
+            return False
+
+        os.remove(image)
+
+        imageFile.save(decrypt_file)
+
+        decrypt_file = "/".join(decrypt_file.split("/")[1:])
+
+        print(decrypt_file)
+
+        return decrypt_file
